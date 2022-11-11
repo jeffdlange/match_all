972bf64463ef3019e089d804b105b511a526da9c
Jeff Lange <jeffersonian.lange@gmail.com>
2022-11-11 00:19:24 -0500

    update gemspec

--- lib/match_all.rb
+++ lib/match_all.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require_relative "match_all/version"
+require_relative "ruby_core_extensions/string/match_all"
 
-module MatchAll
-  class Error < StandardError; end
-  # Your code goes here...
-end
+include RubyCoreExtensions::String::MatchAll

b9b96a40abffef50b1b9d4767bfe7bfa727bbb1c
Jeff Lange <jeffersonian.lange@gmail.com>
2022-11-11 00:19:47 -0500

    include core exts

--- lib/match_all.rb
+++ lib/match_all.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "match_all/version"
+require_relative "ruby_core_extensions/string/match_all"
 
 module MatchAll
   class Error < StandardError; end
-  # Your code goes here...
+
+  include RubyCoreExtensions::String::MatchAll
 end
